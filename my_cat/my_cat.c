ad9d0b7fd5d053182468e8ae6e5bb6f664932fe2
squid <raspberryratman@gmail.com>
2026-08-09 21:03:02 -0600

    moved version, help, invald function call and exit to main

--- my_cat/my_cat.c
+++ my_cat/my_cat.c
@@ -1,12 +1,11 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
 /* Copying the features of cat cli program */
 // will contain CLI flags
-typedef struct 
-{
+typedef struct {
   bool number_lines;
   bool number_nonblank_lines;
   bool show_ends;
@@ -19,32 +18,44 @@ typedef struct
   bool exit_early;
 } Options;
 
-typedef struct 
-{
-  char ** files;
+typedef struct {
+  char **files;
   size_t file_count;
 } File_List;
 
-void print_file(FILE *fp, Options * flag, int * line_number);
-void cleanup(char *** file);
-FILE * open_file(char *filepath);
-void parse_flags(char * flag, Options *flags); 
-void parse_files(char * file_name, File_List * files);
-void parse_arguments(int size, char * arg_list[], File_List * files, Options * flags);
-void print_help(char * program_name); 
-void print_version(char * program_name);
+void print_file(FILE *fp, Options *flag, int *line_number);
+void cleanup(char ***file);
+FILE *open_file(char *filepath);
+void parse_flags(char *flag, Options *flags);
+void parse_files(char *file_name, File_List *files);
+void parse_arguments(int size, char **argv, File_List *files, Options *flags);
+void print_help(char *program_name);
+void print_version(char *program_name);
 
-int main(int argc, char *argv[]) 
-{
+int main(int argc, char *argv[]) {
   int line_number = 1;
   Options flags = {0}; // init all flags to false
-  File_List file_list = {NULL, 0}; // init files array to NULL and file_count to 0
-  
-  parse_arguments(argc, argv, &file_list, &flags);
-  
+  File_List file_list = {NULL,
+                         0}; // init files array to NULL and file_count to 0
+
+  // pass argv[1] because argv[0] is name of program, argc-1 because skip first
+  // program
+  parse_arguments(argc - 1, &argv[1], &file_list, &flags);
+  if (flags.exit_early) {
+    free(file_list.files);
+    if (flags.show_help) {
+      print_help(argv[0]);
+      return 0;
+    } else if (flags.show_version) {
+      print_version(argv[0]);
+      return 0;
+    } else if (flags.invalid_flag) {
+      return 1;
+    }
+  }
+
   // if no arguments or if only flags as arguments
-  if (!(file_list.files)) 
-  {
+  if (!(file_list.files)) {
     print_file(stdin, &flags, &line_number);
     cleanup(&(file_list.files));
     return 0;
@@ -52,32 +63,29 @@ int main(int argc, char *argv[])
 
   // opens each file path and displays them one after another
   FILE *fp;
-  for (size_t i = 0; i < file_list.file_count; i++) 
-  {
-    // if argument is "-" takes input from stdin until EOF (ctrl+D) then continues  to next argument
-    if (strcmp("-", *(file_list.files)) == 0) 
-    {
-      print_file(stdin, &flags, &line_number); 
-    } 
-    else 
-    {
+  for (size_t i = 0; i < file_list.file_count; i++) {
+    // if argument is "-" takes input from stdin until EOF (ctrl+D) then
+    // continues  to next argument
+    if (strcmp("-", *(file_list.files)) == 0) {
+      print_file(stdin, &flags, &line_number);
+    } else {
       fp = open_file(*(file_list.files));
       if (fp == NULL) {
-        fprintf(stderr, "%s: %s: No such file or directory", argv[0], *(file_list.files));
+        fprintf(stderr, "%s: %s: No such file or directory", argv[0],
+                *(file_list.files));
         continue;
       }
-      print_file(fp, &flags, &line_number); 
+      print_file(fp, &flags, &line_number);
       fclose(fp);
     }
   }
-  
+
   cleanup(&(file_list.files));
   return 0;
 }
 
 // displays the contents of fp to stdout
-void print_file(FILE *fp, Options * flags, int * line_number) 
-{
+void print_file(FILE *fp, Options *flags, int *line_number) {
   if (fp == NULL) {
     return;
   }
@@ -85,69 +93,53 @@ void print_file(FILE *fp, Options * flags, int * line_number)
   int c;
   int newline_count = 0;
   bool line_start = true;
-  while ((c = fgetc(fp)) != EOF) 
-  {
+  while ((c = fgetc(fp)) != EOF) {
 
-    if (flags->squeeze_blank) 
-    {
-      if (c == '\n') 
-      {
-        if (newline_count < 2) 
-        {
+    if (flags->squeeze_blank) {
+      if (c == '\n') {
+        if (newline_count < 2) {
           newline_count++;
         } else {
-          while ((c = fgetc(fp)) == '\n') 
-          {
+          while ((c = fgetc(fp)) == '\n') {
             continue;
           }
           ungetc(c, fp);
           continue;
         }
-      }
-      else 
-      {
+      } else {
         newline_count = 0;
       }
     }
-    if (line_start)
-    {
-      if (flags->number_lines) 
-      {
+    if (line_start) {
+      if (flags->number_lines) {
         printf("%6d\t", (*line_number)++);
         line_start = false;
-      } else if (flags->number_nonblank_lines && c != '\n') 
-      {
-        printf("%6d\t",(*line_number)++);
+      } else if (flags->number_nonblank_lines && c != '\n') {
+        printf("%6d\t", (*line_number)++);
         line_start = false;
       }
     }
 
-    if (c == '\n') 
-    {
+    if (c == '\n') {
       line_start = true;
     }
 
-    if (flags->show_ends && (c == 10 || c == 13)) 
-    {
-      if (c == 10) 
-      {
+    if (flags->show_ends && (c == 10 || c == 13)) {
+      if (c == 10) {
         printf("$\n");
         continue;
-      } else if (c == 13) 
-      {
+      } else if (c == 13) {
         printf("^%c", c + 64);
         continue;
       }
     }
-    if (flags->show_tabs && c == 9) 
-    {
-      printf("^%c", c+64);
+    if (flags->show_tabs && c == 9) {
+      printf("^%c", c + 64);
       continue;
     }
 
-    if (flags->show_nonprinting && c < 32 && !(c == 9 || c == 10)) 
-    {
-      printf("^%c", c+64);
+    if (flags->show_nonprinting && c < 32 && !(c == 9 || c == 10)) {
+      printf("^%c", c + 64);
       continue;
     }
 
@@ -155,8 +147,7 @@ void print_file(FILE *fp, Options * flags, int * line_number)
   }
 }
 
-FILE * open_file(char *filepath) 
-{
+FILE *open_file(char *filepath) {
   FILE *fp = fopen(filepath, "r");
   if (fp == NULL) {
     perror(filepath);
@@ -166,136 +157,112 @@ FILE * open_file(char *filepath)
 }
 
 // goes through the input and sets and flag in the CL args
-void parse_flags(char * flag, Options *flags) 
-{
+void parse_flags(char *flag, Options *flags) {
   if (flag[0] != '-') {
     fprintf(stderr, "Invalid flag %s, exiting\n", flag);
     flags->invalid_flag = true;
     flags->exit_early = true;
   }
 
-  if (flag[1] == '-') 
-  {
-    if (strcmp(flag, "--help") == 0) 
-    {
+  if (flag[1] == '-') {
+    if (strcmp(flag, "--help") == 0) {
       flags->show_help = true;
       flags->exit_early = true;
-    } 
-    else if (strcmp(flag, "--version") == 0) 
-    {
+      return; // exit early
+    } else if (strcmp(flag, "--version") == 0) {
       flags->show_version = true;
       flags->exit_early = true;
-    } 
-    else if (strcmp(flag, "--number") == 0) 
-    {
+      // exit early
+      return;
+    } else if (strcmp(flag, "--number") == 0) {
       flags->number_lines = true;
-    } 
-    else if (strcmp(flag,"--show-all") == 0)  
-    {
+    } else if (strcmp(flag, "--show-all") == 0) {
       flags->show_ends = true;
       flags->show_tabs = true;
       flags->show_nonprinting = true;
-    } 
-    else if (strcmp(flag, "--number-nonblank") == 0) 
-    {
+    } else if (strcmp(flag, "--number-nonblank") == 0) {
       flags->number_nonblank_lines = true;
-    } 
-    else if (strcmp(flag, "--show-ends") == 0) 
-    {
+    } else if (strcmp(flag, "--show-ends") == 0) {
       flags->show_ends = true;
-    } 
-    else if (strcmp(flag, "--squeeze-blank") == 0) 
-    {
+    } else if (strcmp(flag, "--squeeze-blank") == 0) {
       flags->squeeze_blank = true;
-    } 
-    else if (strcmp(flag, "--show-tabs") == 0) 
-    {
+    } else if (strcmp(flag, "--show-tabs") == 0) {
       flags->show_tabs = true;
-    } 
-    else if (strcmp(flag, "--show-nonprinting") == 0) 
-    {
+    } else if (strcmp(flag, "--show-nonprinting") == 0) {
       flags->show_nonprinting = true;
-    } 
-    else if (flag[1] == '-') 
-    {
+    } else if (flag[1] == '-') {
       fprintf(stderr, "my_cat: invalid option -- '%s'\n", flag);
       fprintf(stderr, "Try my_cat --help for more information.\n");
       flags->invalid_flag = true;
       flags->exit_early = true;
+      return;
     }
   }
-  
+
   int i = 1;
-  while (flag[i] != '\0') 
-  {
-    switch (flag[i]) 
-    {
-      case 'n':
-        flags->number_lines = true;
-        break;
-      case 'A':
-        flags->show_ends = true;
-        flags->show_tabs = true;
-        flags->show_nonprinting = true;
-        break;
-      case 'b':
-        flags->number_nonblank_lines = true;
-        break;
-      case 'e':
-        flags->show_ends = true;
-        flags->show_nonprinting = true;
-        break;
-      case 'E':
-        flags->show_ends = true;
-        break;
-      case 's':
-        flags->squeeze_blank = true;
-        break;
-      case 't':
-        flags->show_nonprinting = true;
-        flags->show_tabs = true;
-        break;
-      case 'T':
-        flags->show_tabs = true;
-        break;
-      case 'v':
-        flags->show_nonprinting = true;
-        break;
-      case 'u':
-        break;
-      default:
-        fprintf(stderr, "my_cat: invalid option -- '%c'\n", flag[i]);
-        fprintf(stderr, "Try my_cat --help for more information.\n");
-        flags->invalid_flag = true;
-        flags->exit_early = true;
+  while (flag[i] != '\0') {
+    switch (flag[i]) {
+    case 'n':
+      flags->number_lines = true;
+      break;
+    case 'A':
+      flags->show_ends = true;
+      flags->show_tabs = true;
+      flags->show_nonprinting = true;
+      break;
+    case 'b':
+      flags->number_nonblank_lines = true;
+      break;
+    case 'e':
+      flags->show_ends = true;
+      flags->show_nonprinting = true;
+      break;
+    case 'E':
+      flags->show_ends = true;
+      break;
+    case 's':
+      flags->squeeze_blank = true;
+      break;
+    case 't':
+      flags->show_nonprinting = true;
+      flags->show_tabs = true;
+      break;
+    case 'T':
+      flags->show_tabs = true;
+      break;
+    case 'v':
+      flags->show_nonprinting = true;
+      break;
+    case 'u':
+      break;
+    default:
+      fprintf(stderr, "my_cat: invalid option -- '%c'\n", flag[i]);
+      fprintf(stderr, "Try my_cat --help for more information.\n");
+      flags->invalid_flag = true;
+      flags->exit_early = true;
     }
     i++;
   }
   // -b flag overrides -n
-  if (flags->number_nonblank_lines) 
-  {
+  if (flags->number_nonblank_lines) {
     flags->number_lines = false;
   }
 }
 
-void parse_files(char * file_name, File_List * file_list) 
-{
-  if (!(file_list->files)) 
-  {
+void parse_files(char *file_name, File_List *file_list) {
+  if (!(file_list->files)) {
     // allocate room for one pointer
     file_list->files = malloc(sizeof(file_list->files));
-    if (!(file_list->files)) 
-    {
+    if (!(file_list->files)) {
       perror(file_name);
       exit(EXIT_FAILURE);
     }
-  } 
-  else
-  {
+  } else {
     // realloc for another pointer
-    char ** file_list_temp = realloc(file_list->files, (file_list->file_count + 1) * sizeof(file_list->files));
-    if (*file_list_temp)
-    {
+    char **file_list_temp =
+        realloc(file_list->files,
+                (file_list->file_count + 1) * sizeof(file_list->files));
+    if (*file_list_temp) {
       perror(file_name);
       cleanup(&(file_list->files));
       exit(EXIT_FAILURE);
@@ -306,77 +273,60 @@ void parse_files(char * file_name, File_List * file_list)
   file_list->files[(file_list->file_count)++] = file_name;
 }
 
-void parse_arguments(int size, char * argv[], File_List * file_list, Options * flags) 
-{
-  int i;
-  int exit_status; // exit return code depending on valid or invalid flag
-  
-  for (i = 1; i < size; i++) 
-  {
+void parse_arguments(int size, char **argv, File_List *file_list,
+                     Options *flags) {
+  for (int i = 0; i < size; i++) {
 
-    if (argv[i][0] == '-' && argv[i][1] != '\0' ) 
-    {
+    if (argv[i][0] == '-' && argv[i][1] != '\0') {
       parse_flags(argv[i], flags);
 
-      if (flags->exit_early) // some flags or invalid option cause program to exit early
+      if (flags->exit_early) // some flags or invalid option cause program to
+                             // exit early
       {
-        if (flags->show_help)
-        {
-          print_help(argv[0]);
-          exit_status = 0;
-        }
-        else if (flags->show_version)
-        {
-          print_version(argv[0]);
-          exit_status = 0;
-        }
-        else if (flags->invalid_flag)
-        {
-          exit_status = 1;
-        }
-        free(file_list->files);
-        exit(exit_status);
+        // return early to main
+        return;
       }
-    } 
-    else 
-  {
+    } else {
       parse_files(argv[i], file_list);
     }
   }
 }
 
-void print_help(char * program_name) {
+void print_help(char *program_name) {
   printf("Usage: %s [OPTION]... [FILE]...\n", program_name);
   printf("Concatenate File(s) to standard output.\n");
   printf("\nWith no FILE, or when FILE is -, read standard input.\n");
-  printf("\n   %-25sequivalent to -vET\n","-A, --show-all");
-  printf("   %-25snumber nonempty output lines, overrides -n\n", "-b, --number-nonblank");
+  printf("\n   %-25sequivalent to -vET\n", "-A, --show-all");
+  printf("   %-25snumber nonempty output lines, overrides -n\n",
+         "-b, --number-nonblank");
   printf("   %-25sequivalent to -vE\n", "-e");
   printf("   %-25sdisplay $ at end of each line\n", "-E, --show-ends");
   printf("   %-25snumber all output lines\n", "-n, --number");
-  printf("   %-25ssuppress repeated empty output lines\n", "-s, --squeeze-blank");
+  printf("   %-25ssuppress repeated empty output lines\n",
+         "-s, --squeeze-blank");
   printf("   %-25sequivalent to -vT\n", "-t");
   printf("   %-25sdisplay TAB characters as ^I\n", "-T, --show-tabs");
   printf("   %-25s(ignored)\n", "-u");
-  printf("   %-25suse ^ and M- notation, except for LFD and TAB\n", "-v, --show-nonprinting");
+  printf("   %-25suse ^ and M- notation, except for LFD and TAB\n",
+         "-v, --show-nonprinting");
   printf("\t    %-14sdisplay this help and exit\n", "--help");
   printf("\t    %-14soutput version information and exit\n", "--version");
 
   printf("\nExamples:\n");
-  printf("   %s %-3s Output f's contents, then standard input, then g's contents.\n", program_name, "f - g");
+  printf("   %s %-3s Output f's contents, then standard input, then g's "
+         "contents.\n",
+         program_name, "f - g");
   printf("   %-11s Copy standard input to standard output.\n", program_name);
 
-  printf("\nThis is a copy of cat, a GNU coreutils program. It was made to practice programming in C.\n");
+  printf("\nThis is a copy of cat, a GNU coreutils program. It was made to "
+         "practice programming in C.\n");
 }
 
-void print_version(char * program_name) {
+void print_version(char *program_name) {
   printf("%s (clone of GNU coreutils) 9.4", program_name);
   printf("\nNo claim of any copyright or license\n");
   printf("There is NO WARRANTY, to the extent permitted by law.\n");
   printf("\nWritten by Abrasive Squid\n");
 }
 
-
-void cleanup(char *** files) {
-  free(*files);
-}
+void cleanup(char ***files) { free(*files); }
