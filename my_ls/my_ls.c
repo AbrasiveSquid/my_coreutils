fb34ac692e03f95b042417a672a0108032003656
squid <raspberryratman@gmail.com>
2026-08-16 21:22:09 -0600

    name type fixed

--- my_ls/my_ls.c
+++ my_ls/my_ls.c
@@ -1,1 +1,18 @@
 #include "my_ls_helper.h"
+
+int main(int argc, char *argv[])
+{
+  DIR *dp;
+  struct dirent *dirp;
+
+  dp = opendir(argv[1]);
+  dirp = readdir(dp);
+
+  if (!(dirp))
+  {
+    perror(argv[1]);
+    return 1;
+  }
+
+  return 0;
+}
